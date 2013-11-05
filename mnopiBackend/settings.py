cbf9a69e19cc040b61e752a43131c64819f27c0c
alfredolainez <alfredo.lainez@mnopi.com>
2013-11-05 17:28:56 +0100

    static files

--- mnopiBackend/settings.py
+++ mnopiBackend/settings.py
@@ -61,7 +61,9 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/static/'
+import os
+PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
