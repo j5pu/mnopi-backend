91694b3d30bb6ac2aec08f52b9d53e22ca14b138
alfredolainez <alfredo.lainez@mnopi.com>
2013-12-13 15:22:12 +0100

    Transactions to avoid duplicate domains

--- mnopi/opendns.py
+++ mnopi/opendns.py
@@ -1,8 +1,8 @@
 from lxml import html
-
 import urllib2 #TODO: pensar si viene mejor el modulo Request+
-
 from models import CategorizedDomain
+from django.db import transaction
+
 APPROVED_CATEGORIES_QUERY = "//td[text()=\"Approved\"]/parent::node()/td/b/text()"
 
 CATEGORIES = { "Academic Fraud": "Academic Fraud",
@@ -77,25 +77,27 @@ def getCategories(domain, add_if_does_not_exist=True):
     """
 
     # First, search domain in pre-fetched categories. If the domain was not previously saved, query OpenDns
+    # Transaction protects from other threads creating same domain
     # Todo: habra que hacer algo para que con el tiempo se vuelva a hacer peticion a opendns
     approved_categories = []
-    if CategorizedDomain.objects.filter(domain=domain).exists():
-        approved_categories = CategorizedDomain.objects.get(domain=domain).categories.all()
-        approved_categories = [cat.name for cat in approved_categories]
-    elif add_if_does_not_exist:
-        page = urllib2.urlopen("http://domain.opendns.com/" + domain)
-        htmlCode = "".join(page.readlines()).replace("\n", "").replace("\t", "")
-        xmlTree = html.fromstring(htmlCode)
-        approved_categories = xmlTree.xpath(APPROVED_CATEGORIES_QUERY)
+    with transaction.commit_on_success:
+        if CategorizedDomain.objects.filter(domain=domain).exists():
+            approved_categories = CategorizedDomain.objects.get(domain=domain).categories.all()
+            approved_categories = [cat.name for cat in approved_categories]
+        elif add_if_does_not_exist:
+            page = urllib2.urlopen("http://domain.opendns.com/" + domain)
+            htmlCode = "".join(page.readlines()).replace("\n", "").replace("\t", "")
+            xmlTree = html.fromstring(htmlCode)
+            approved_categories = xmlTree.xpath(APPROVED_CATEGORIES_QUERY)
 
-        # Check existence and adapt to our categories
-        for category in approved_categories:
-            if category not in CATEGORIES.keys():
-                raise OpenDNS_DOMException()
-        approved_categories = [CATEGORIES[cat] for cat in approved_categories]
+            # Check existence and adapt to our categories
+            for category in approved_categories:
+                if category not in CATEGORIES.keys():
+                    raise OpenDNS_DOMException()
+            approved_categories = [CATEGORIES[cat] for cat in approved_categories]
 
-        cat_domain = CategorizedDomain(domain=domain)
-        cat_domain.save()
-        cat_domain.add_categories(approved_categories)
+            cat_domain = CategorizedDomain(domain=domain)
+            cat_domain.save()
+            cat_domain.add_categories(approved_categories)
 
     return approved_categories
